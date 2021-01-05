00a5d8d5363e19c866dc1948e700901f07bd1516
Tad Scritchfield <tadscritch@gmail.com>
2021-01-04 22:58:51 -0600

    Initial Commit

---
+++ SortableList/SortableListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SortableListApp.swift
+//  SortableList
+//
+//  Created by Tad Scritchfield on 1/4/21.
+//
+
+import SwiftUI
+
+@main
+struct SortableListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
